25d9947a259830eaae03ad1ebd4c5f4541b17908
nattanagaraju <natta_nagaraju@yahoo.co.in>
2015-08-01 06:45:18 -0700

    test

--- src/main/java/org/hack/TUserService.java
+++ src/main/java/org/hack/TUserService.java
@@ -26,8 +26,6 @@ public class TUserService {
 	public Viewable findById() {
 		TLogger.logInfo("Rest Service called...");
 		request.setAttribute("message", "I am called..");
-		MySQLDAO dao = new MySQLDAO();
-		dao.insertdata();
 		TLogger.reportLog("Completed..");
 		return new Viewable("/helo.html");
 	}
