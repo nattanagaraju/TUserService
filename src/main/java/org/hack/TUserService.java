973762449ef6e3ecb38ea3a8c6e03ddab88d576d
nattanagaraju <natta_nagaraju@yahoo.co.in>
2015-08-01 08:16:28 -0700

    tesest

--- src/main/java/org/hack/TUserService.java
+++ src/main/java/org/hack/TUserService.java
@@ -1,17 +1,21 @@
 package org.hack;
 
+import java.util.Map;
 import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import org.hack.dao.MySQLDAO;
 import org.hack.util.TLogger;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.sun.jersey.api.container.httpserver.HttpServerFactory;
 import com.sun.jersey.api.view.Viewable;
 
@@ -21,13 +25,17 @@ public class TUserService {
 	HttpServletRequest request;
 
 	@GET 
-	@Path("/test")
-	@Produces(MediaType.TEXT_HTML)
-	public Viewable findById() {
+	@Path("/getUser")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getUser(@QueryParam("email") String email) {
+		String response = null;
 		TLogger.logInfo("Rest Service called...");
-		request.setAttribute("message", "I am called..");
-		TLogger.reportLog("Completed..");
-		return new Viewable("/helo.html");
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		MySQLDAO dao = new MySQLDAO();
+		Map<String, Object> resp = dao.executeQuery("select * from users where email='"+email+"'");
+		response = gson.toJson(resp); 
+		TLogger.reportLog("Completed.."+response);
+		return email;
 	}
 	
 }
